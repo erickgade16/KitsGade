722fbc18db5871270ed6e20533d3c0cada0d7187
erickgade16 <erickgade16>
2023-08-23 21:50:07 -0300

    Populando tabela categoria no banco de dados

---
+++ Migrations/20230824004349_PopularCategoria.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KitsGade.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategopriaNome)"+
+                "VALUES('Tênis')");
+            migrationBuilder.Sql("INSERT INTO Categorias(CategopriaNome)" +
+               "VALUES('Camisa')");
+            migrationBuilder.Sql("INSERT INTO Categorias(CategopriaNome)" +
+               "VALUES('Moletom')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+
+        }
+    }
+}
