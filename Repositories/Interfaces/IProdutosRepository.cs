8edfd854e99977596be6a88f5197fa3e1846773c
erickgade16 <erickgade16>
2023-08-25 21:17:06 -0300

    Implementando o padrão Repository para lanches

---
+++ Repositories/Interfaces/IProdutosRepository.cs
@@ -0,0 +1,11 @@
+﻿using KitsGade.Models;
+
+namespace KitsGade.Repositories.Interfaces
+{
+    public interface IProdutosRepository
+    {
+        IEnumerable<Produto> Produtos { get; }
+        IEnumerable<Produto> ProdutosPreferidos { get; }
+        Produto GetProdutoById(int produtoId);
+    }
+}
---
+++ Repositories/ProdutosRepository.cs
@@ -0,0 +1,15 @@
+﻿using KitsGade.Context;
+using KitsGade.Repositories.Interfaces;
+
+namespace KitsGade.Repositories
+{
+    public class ProdutosRepository:IProdutosRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
