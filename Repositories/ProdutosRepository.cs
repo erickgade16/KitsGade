ce89ba7ef49455dd4ec69778a3228ec69ed9de2a
erickgade16 <erickgade16>
2023-08-28 20:10:38 -0300

    Criando repositorio para Produtos

--- Repositories/ProdutosRepository.cs
+++ Repositories/ProdutosRepository.cs
@@ -1,15 +1,26 @@
 ﻿using KitsGade.Context;
+using KitsGade.Models;
 using KitsGade.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace KitsGade.Repositories
 {
-    public class ProdutosRepository:IProdutosRepository
+    public class ProdutosRepository : IProdutosRepository
     {
         private readonly AppDbContext _context;
 
         public ProdutosRepository(AppDbContext context)
         {
             _context = context;
+        }
+        public IEnumerable<Produto> Produtos => _context.Produtos.Include(c => c.Categoria);
+        public IEnumerable<Produto> ProdutosPreferidos => _context.Produtos.
+            Where(l => l.IsProdutoPreferido).
+            Include(c => c.Categoria);
+
+        public Produto GetProdutoById(int produtoId)
+        {
+            return _context.Produtos.FirstOrDefault(l => l.ProdutoId == produtoId);
         }
     }
 }
