135697a70b7b2e3cea6a87c9dd9b9378d3bdc8bc
erickgade16 <erickgade16>
2023-08-29 11:03:58 -0300

    Criando o controller e view para exibir produtos

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,21 @@
+﻿using KitsGade.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KitsGade.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutosRepository _produtosRepository;
+
+        public ProdutoController(IProdutosRepository produtosRepository)
+        {
+            _produtosRepository = produtosRepository;
+        }
+
+        public IActionResult List()
+        {
+            var produtos = _produtosRepository.Produtos;
+            return View(produtos);
+        }
+    }
+}
