13898b1c2175222fbd4926291486b429c744d852
erickgade16 <erickgade16>
2023-09-25 18:38:13 -0300

    Adicionar Itens ao Carrinho de Compras

--- Models/CarrinhoCompra.cs
+++ Models/CarrinhoCompra.cs
@@ -1,6 +1,60 @@
-﻿namespace KitsGade.Models
+﻿using KitsGade.Context;
+
+namespace KitsGade.Models
 {
     public class CarrinhoCompra
     {
+        private readonly AppDbContext _context;
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItems  { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            //define uma sessão
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //obtem um serviço do tipo do nosso contexto
+            var context = services.GetService<AppDbContext>();
+
+            //obtem ou gera o Id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId")??Guid.NewGuid().ToString();
+
+            //atribui o id do carrinho na sessão
+            session.SetString("CarrinhoId", carrinhoId);
+
+            //retorna o carrinho com o contexto e o id atribuido ou obtido
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId,
+            };
+        }
+        public void AdicionarAoCarrinho(Produto produto)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+                s => s.Produto.ProdutoId == produto.ProdutoId &&
+                s.CarrinhoCompraId == CarrinhoCompraId);
+                
+            if(carrinhoCompraItem == null)
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Produto = produto,
+                    Quantidade = 1
+
+                };
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+            }
+            else
+            {
+                carrinhoCompraItem.Quantidade++;
+            }
+            _context.SaveChanges();
+        }
+
     }
 }
