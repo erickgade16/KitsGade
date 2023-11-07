ca7ce120032ce584c7f6945a3b0e0edd3e65c5b7
Erick Gadelha da Silva <erickgade12321@gmail.com>
2023-11-07 14:23:11 -0300

    Criando aba de Contato com TagHelpers

---
+++ TagHelpers/EmailTagHelpers.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace KitsGade.TagHelpers
+{
+    [HtmlTargetElement("email")]
+    public class EmailTagHelpers : TagHelper
+    {
+        public string Endereco { get; set; }
+        public string Conteudo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Endereco);
+            output.Content.SetContent(Conteudo);
+        }
+    }
+}
