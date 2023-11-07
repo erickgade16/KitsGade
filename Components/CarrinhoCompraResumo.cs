a5c83fb5f1d36509faba1b8ae26d0ec483ab57b6
Erick Gadelha da Silva <erickgade12321@gmail.com>
2023-11-07 14:22:37 -0300

    Criando uma ViewComponent para exibir detalhes do carrinho

---
+++ Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,31 @@
+﻿using KitsGade.Models;
+using KitsGade.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KitsGade.Components
+{
+    public class CarrinhoCompraResumo:ViewComponent
+    {
+        readonly private CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal(),
+            };
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
