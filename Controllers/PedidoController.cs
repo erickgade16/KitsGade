647ace6deb1af0bf500ad0d29756fe0510c9215f
Erick Gadelha da Silva <erickgade12321@gmail.com>
2023-12-16 19:56:10 -0300

    Criando o método Action Checkout

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -23,7 +23,51 @@ namespace KitsGade.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido) 
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0;
+
+            //obtem  os items do carrinho de compra
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            //verificar se existem items no carrinho
+            if (_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio.");
+            }
+
+            //calcular o total de itens e total do pedido
+            foreach (var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Produto.Preco * item.Quantidade);
+
+            }
+
+
+            //atribui os valores obtidos ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+
+            //valida os dados do pedido
+            if(ModelState.IsValid )
+            {
+                //cria pedido e os detalhes
+                _pedidoRepository.CriarPedido(pedido);
+
+                //definir mensagens ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido!";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraItens();
+
+                //limpar carrinho
+                _carrinhoCompra.LimparCarrinho();
+
+                //Exibe a view com  dados do cliente  e do pedido
+                return View("~/View/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
+
         }
     }
 }
